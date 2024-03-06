d2a21fdf583ccb3e9b6b5412a60cbaab2438a950
Vivek Choudhary <vivek.choudhary.0022@gmail.com>
2024-03-06 12:58:13 +0530

    urls and views update

--- backend/quiz_backend/urls.py
+++ backend/quiz_backend/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('test/', views.TestList.as_view()),
     path('choice/', views.ChoiceList.as_view()),
     path('createlanguage/', views.CreateLanguageView.as_view(), name='create_language'),
+    path('languages/<int:pk>/delete/', views.LanguageDeleteView.as_view(), name='language-delete'),
+    path('languages/<int:pk>/update/', views.LanguageUpdateView.as_view(), name='language-update'),
+    path('add_question/', views.CreateQuestionview.as_view(), name='Create_question'),
+    path('add_choice/', views.CreateChoiceView.as_view(), name='Create_Choice'),
+
     
 ]
--- backend/quiz_backend/views.py
+++ backend/quiz_backend/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from .serializers import Registration_serializer , Language_serializer , QuestionSerializer ,  TestSerializer , Choice_serializer
-from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView , CreateAPIView , UpdateAPIView , DestroyAPIView
 # Create your views here.
 from .models import Registration , Language , Question ,Test , Choice
@@ -35,8 +34,41 @@ class TestList(ListAPIView):
         language_id = self.request.query_params.get('language')
         return Test.objects.filter(language_id=language_id)
 
-# POST data TO  SERVER
+
+# Create , Delete and Update operations for Language
+# POST data from Database
 class CreateLanguageView(CreateAPIView):
     queryset = Language.objects.all()
     serializer_class =  Language_serializer
-       
+
+# Update data from Database
+class LanguageUpdateView(UpdateAPIView):
+    queryset = Language.objects.all()
+    serializer_class =  Language_serializer
+
+# Delete data from Database
+class LanguageDeleteView(DestroyAPIView):
+    queryset = Language.objects.all()
+    serializer_class = Language_serializer
+
+
+
+# Create operations for Questions
+
+# POST data to Database
+class CreateQuestionview(CreateAPIView):
+    serializer_class = QuestionSerializer
+
+    def get_queryset(self):
+        language_id = self.request.query_params.get('language')
+        return Question.objects.filter(language_id=language_id)
+
+
+
+# Operations for Choice
+
+# POST data to Choice
+class  CreateChoiceView(CreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = Choice_serializer
+    
